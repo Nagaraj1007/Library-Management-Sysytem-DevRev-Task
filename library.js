a306368b94645708467fcd721604f273242e88b9
Nagaraj <110826131+Nagaraj1007@users.noreply.github.com>
2023-04-18 11:44:34 +0530

    Add files via upload

---
+++ library.js
@@ -0,0 +1,409 @@
+var data = {
+    "data": [
+        {
+          "SNO": 1,
+          "TITLE": "To Kill a Mockingbird",
+          "AUTHOR": "Harper Lee",
+          "SUBJECT": "Fiction",
+          "PUBLISH DATE": "2005-05-25"
+        },
+        {
+          "SNO": 2,
+          "TITLE": "1984",
+          "AUTHOR": "George Orwell",
+          "SUBJECT": "Science",
+          "PUBLISH DATE": "2015-10-15"
+        },
+        {
+          "SNO": 3,
+          "TITLE": "The Great Gatsby",
+          "AUTHOR": "F. Scott Fitzgerald",
+          "SUBJECT": "Fiction",
+          "PUBLISH DATE": "2012-12-10"
+        },
+        {
+          "SNO": 4,
+          "TITLE": "The Notebook",
+          "AUTHOR": "Nicholas Sparks",
+          "SUBJECT": "Romance",
+          "PUBLISH DATE": "2019-11-12"
+        },
+        {
+          "SNO": 5,
+          "TITLE": "The Rosie Project",
+          "AUTHOR": "Graeme Simsion",
+          "SUBJECT": "Romantic Comedy",
+          "PUBLISH DATE": "2009-12-25"
+        },
+        {
+          "SNO": 6,
+          "TITLE": "The Wedding Date",
+          "AUTHOR": "Jasmine Guillory",
+          "SUBJECT": "Romantic Comedy",
+          "PUBLISH DATE": "2018-08-01"
+        },
+        {
+          "SNO": 7,
+          "TITLE": "The Alchemist",
+          "AUTHOR": "Paulo Coelho",
+          "SUBJECT": "Personal Development",
+          "PUBLISH DATE": "2012-12-10"
+        },
+        {
+          "SNO": 8,
+          "TITLE": "Man's Search for Meaning",
+          "AUTHOR": "Viktor E. Frankl",
+          "SUBJECT": "Psychology & Philosophy",
+          "PUBLISH DATE": "2016-06-07"
+        },
+        {
+          "SNO": 9,
+          "TITLE": "The Power of Now",
+          "AUTHOR": "Eckhart Tolle",
+          "SUBJECT": "Spirituality & Mindfulness",
+          "PUBLISH DATE": "2010-09-08"
+        },
+        {
+          "SNO": 10,
+          "TITLE": "The Diary of a Young Girl",
+          "AUTHOR": "Anne Frank",
+          "SUBJECT": "Biography",
+          "PUBLISH DATE": "2014-07-20"
+        },
+        {
+          "SNO": 11,
+          "TITLE": "The Friendship Factor: How to Get Closer to the People You Care For",
+          "AUTHOR": "Alan Loy McGinnis",
+          "SUBJECT": "Relationships",
+          "PUBLISH DATE": "2016-06-07"
+        },
+        {
+          "SNO": 12,
+          "TITLE": "The Science of Making Friends: Helping Socially Challenged Teens and Young Adults",
+          "AUTHOR": "Elizabeth Laugeson",
+          "SUBJECT": "Psychology",
+          "PUBLISH DATE": "2010-09-08"
+        },
+        {
+          "SNO": 13,
+          "TITLE": "The Like Switch: An Ex-FBI Agent's Guide to Influencing, Attracting, and Winning People Over",
+          "AUTHOR": "Jack Schafer, Marvin Karlins",
+          "SUBJECT": "Social psychology",
+          "PUBLISH DATE": "2016-06-07"
+        },
+        {
+          "SNO": 14,
+          "TITLE": "How to Win Friends and Influence People",
+          "AUTHOR": "Dale Carnegie",
+          "SUBJECT": "Self help",
+          "PUBLISH DATE": "2007-07-22"
+        },
+        {
+          "SNO": 15,
+          "TITLE": "The Self-Care Project: How to Let Go of Frazzle and Make Time for You",
+          "AUTHOR": "Jayne Hardy",
+          "SUBJECT": "Self help",
+          "PUBLISH DATE": "2001-12-25"
+        },
+        {
+          "SNO": 16,
+          "TITLE": "The Da Vinci Code",
+          "AUTHOR": "Dan Brown",
+          "SUBJECT": "Mystery",
+          "PUBLISH DATE": "2015-09-11"
+        },
+        {
+          "SNO": 17,
+          "TITLE": "The Girl with the Dragon Tattoo",
+          "AUTHOR": "Stieg Larsson",
+          "SUBJECT": "Mystery",
+          "PUBLISH DATE": "2019-02-11"
+        },
+        {
+          "SNO": 18,
+          "TITLE": "The Hunger Games",
+          "AUTHOR": "Suzanne Collins",
+          "SUBJECT": "Science Fiction",
+          "PUBLISH DATE": "2001-12-25"
+        },
+        {
+          "SNO": 19,
+          "TITLE": "The Martian",
+          "AUTHOR": "Andy Weir",
+          "SUBJECT": "Science Fiction",
+          "PUBLISH DATE": "2011-10-10"
+        },
+        {
+          "SNO": 20,
+          "TITLE": "Gone Girl",
+          "AUTHOR": "Gillian Flynn",
+          "SUBJECT": "Mystery",
+          "PUBLISH DATE": "2003-02-20"
+        },
+        {
+          "SNO": 21,
+          "TITLE": "The Fault in Our Stars",
+          "AUTHOR": "John Green",
+          "SUBJECT": "Young Adult Fiction",
+          "PUBLISH DATE": "2013-03-19"
+        },
+        {
+          "SNO": 22,
+          "TITLE": "Love and Other Words",
+          "AUTHOR": "Christina Lauren",
+          "SUBJECT": "Romance",
+          "PUBLISH DATE": "2018-07-25"
+        },
+        {
+          "SNO": 23,
+          "TITLE": "The Underground Railroad",
+          "AUTHOR": "Colson Whitehead",
+          "SUBJECT": "Historical Fiction",
+          "PUBLISH DATE": "2012-10-04"
+        },
+        {
+          "SNO": 24,
+          "TITLE": "The Soulmate Equation",
+          "AUTHOR": "Christina Lauren",
+          "SUBJECT": "Romance",
+          "PUBLISH DATE": "2021-05-18"
+        },
+        {
+          "SNO": 25,
+          "TITLE": "The Immortalists",
+          "AUTHOR": "Chloe Benjamin",
+          "SUBJECT": "Fiction",
+          "PUBLISH DATE": "2013-03-19"
+        },
+        {
+          "SNO": 26,
+          "TITLE": "Becoming",
+          "AUTHOR": "Michelle Obama",
+          "SUBJECT": "Autobiography",
+          "PUBLISH DATE": "2019-09-20"
+        },
+        {
+          "SNO": 27,
+          "TITLE": "Little Fires Everywhere",
+          "AUTHOR": "Celeste Ng",
+          "SUBJECT": "Fiction",
+          "PUBLISH DATE": "2012-10-04"
+        },
+        {
+          "SNO": 28,
+          "TITLE": "Bridget Jones's Diary",
+          "AUTHOR": "Helen Fielding",
+          "SUBJECT": "Comedy",
+          "PUBLISH DATE": "2015-08-06"
+        },
+        {
+          "SNO": 29,
+          "TITLE": "Bossypants",
+          "AUTHOR": "Tina Fey",
+          "SUBJECT": "Autobiography, Comedy",
+          "PUBLISH DATE": "2013-08-22"
+        },
+        {
+          "SNO": 30,
+          "TITLE": "Good Omens",
+          "AUTHOR": "Terry Pratchett, Neil Gaiman",
+          "SUBJECT": "Fantasy, Comedy",
+          "PUBLISH DATE": "2010-11-17"
+        }
+    ]
+};
+//data.data.sort((a, b) => a.TITLE.localeCompare(b.TITLE));     Sorting data based on Title (localCompare compares two strings and declare Negative if a<b and Positive if a>b and Zero if a==0) 
+var table = document.getElementById("mytable");
+var tbody = table.getElementsByTagName("tbody")[0];
+
+for (var i = 0; i < 10; i++) {
+    var row = tbody.insertRow(i);
+    row.insertCell(0).innerHTML = i+1;
+    row.insertCell(1).innerHTML = data.data[i].TITLE;
+    row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+    row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+    row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+}
+const dropdownMenu = document.querySelector('.dropdown-menu');
+var count=0;
+let flatpickrInstance;
+// add an event listener to the dropdown menu
+dropdownMenu.addEventListener('click', function(event) {
+  // get the clicked dropdown item
+  const clickedItem = event.target;
+
+  // get the text content of the clicked dropdown item
+  const selectedItem = clickedItem.textContent.trim();
+ if(selectedItem==="Title")
+ {
+  document.getElementById("searchInput").value="";
+  count=1;
+  document.getElementById("bt").innerHTML="Title";
+  if (flatpickrInstance) {
+    flatpickrInstance.destroy();
+  }
+ }
+ else if(selectedItem==="Author")
+ {
+  document.getElementById("searchInput").value="";
+  count=2;
+  document.getElementById("bt").innerHTML="Author";
+    flatpickrInstance.destroy();
+  
+ }
+ else if(selectedItem==="Subject")
+ {
+  document.getElementById("searchInput").value="";
+  count=3;
+  document.getElementById("bt").innerHTML="Subject";
+  if (flatpickrInstance) {
+    flatpickrInstance.destroy();
+  }
+ }
+ else if(selectedItem==="Publish Date")
+ {
+  document.getElementById("searchInput").value="";
+  count=4;
+  document.getElementById("bt").innerHTML="Publish Date";
+  flatpickrInstance=flatpickr("#searchInput", {
+    dateFormat: "Y-m-d",
+    maxDate: "today"
+  });
+ } 
+console.log(count);
+});
+
+function searchBooks() {
+    var input = document.getElementById("searchInput").value;
+    var found = false;
+    var c=1;
+    tbody.innerHTML = ''; // clear existing table rows
+   if(count==1){
+    for (var i = 0; i < data.data.length; i++) {
+      if (data.data[i].TITLE.toLowerCase().includes(input.toLowerCase())) {
+        found = true;
+        var row = tbody.insertRow();
+        row.insertCell(0).innerHTML = c;
+        row.insertCell(1).innerHTML = data.data[i].TITLE;
+        row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+        row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+        row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+        c++;
+      }
+    }
+    if (!found) {
+      alert("Not available");
+    }
+  }
+  else if(count==2){
+    for (var i = 0; i < data.data.length; i++) {
+      if (data.data[i].AUTHOR.toLowerCase().includes(input.toLowerCase())) {
+        found = true;
+        var row = tbody.insertRow();
+        row.insertCell(0).innerHTML = c;
+        row.insertCell(1).innerHTML = data.data[i].TITLE;
+        row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+        row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+        row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+        c++;
+      }
+    }
+    if (!found) {
+      alert("Not available");
+    }
+  }
+  else if(count==3){
+    for (var i = 0; i < data.data.length; i++) {
+      if (data.data[i].SUBJECT.toLowerCase().includes(input.toLowerCase())) {
+        found = true;
+        var row = tbody.insertRow();
+        row.insertCell(0).innerHTML = c;
+        row.insertCell(1).innerHTML = data.data[i].TITLE;
+        row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+        row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+        row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+        c++;
+      }
+    }
+    if (!found) {
+      alert("Not available");
+    }
+  }
+  else if(count==4){
+    for (var i = 0; i < data.data.length; i++) {
+      if (data.data[i]["PUBLISH DATE"].toLowerCase().includes(input.toLowerCase())) {
+        found = true;
+        var row = tbody.insertRow();
+        row.insertCell(0).innerHTML = c;
+        row.insertCell(1).innerHTML = data.data[i].TITLE;
+        row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+        row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+        row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+        c++;
+      }
+    }
+    if (!found) {
+      alert("Not available");
+    }
+  }
+  }
+function changeActivePage(pageNum) {
+    // Get all the page links
+    var pageLinks = document.querySelectorAll('.pagination .page-link');
+    // Loop through the page links
+    for (var i = 0; i < pageLinks.length; i++) {
+      // Remove the "active" class from all links
+      pageLinks[i].parentNode.classList.remove('active');
+  
+      // If this is the link for the clicked page, add the "active" class
+      if (parseInt(pageLinks[i].innerHTML) === pageNum) {
+        pageLinks[i].parentNode.classList.add('active');
+      }
+    }
+    if (pageNum === 1) {
+      document.getElementById("searchInput").value="";
+        var table = document.getElementById("mytable");
+        var tbody = table.getElementsByTagName("tbody")[0];
+        tbody.innerHTML = ""; // clear the table body
+        
+        for (var i = 0; i < 10; i++) {
+          var row = tbody.insertRow(i); // adjust for index starting at 0
+          row.insertCell(0).innerHTML = i+1;
+          row.insertCell(1).innerHTML = data.data[i].TITLE;
+          row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+          row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+          row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+        }
+      }
+    else if (pageNum === 2) {
+      document.getElementById("searchInput").value="";
+      var table = document.getElementById("mytable");
+      var tbody = table.getElementsByTagName("tbody")[0];
+      tbody.innerHTML = ""; // clear the table body
+      
+      for (var i = 10; i < 20; i++) {
+        var row = tbody.insertRow(i-10); // adjust for index starting at 0
+        row.insertCell(0).innerHTML = i+1;
+        row.insertCell(1).innerHTML = data.data[i].TITLE;
+        row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+        row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+        row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+      }
+    } else if (pageNum === 3) {
+      document.getElementById("searchInput").value="";
+      var table = document.getElementById("mytable");
+      var tbody = table.getElementsByTagName("tbody")[0];
+      tbody.innerHTML = ""; // clear the table body
+      
+      for (var i = 20; i < 30; i++) {
+        var row = tbody.insertRow(i-20); // adjust for index starting at 0
+        row.insertCell(0).innerHTML = i+1;
+        row.insertCell(1).innerHTML = data.data[i].TITLE;
+        row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+        row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+        row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+      }
+    }
+    
+  }
+  
